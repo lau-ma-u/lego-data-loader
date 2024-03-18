e4ad80d0bcfb1504a30bf76c02e281ed404c2dc6
Laura U <laura.uskola@gmail.com>
2024-03-18 14:57:05 +0200

    Updated the structure. Added data.py and plots.py. Added functionality for drawing and saving plots.

---
+++ data.py
@@ -0,0 +1,127 @@
+import requests
+import pandas as pd
+import keys
+
+
+# API URL's
+URL_REBRICK = "https://rebrickable.com"
+URL_BRICKSET = "https://brickset.com/api/v3.asmx"
+
+
+def get_mocs(code, parts, params):
+    """Writes a file containing the info of the MOCs of a specific Lego set.
+    Returns a tuple containing the number of MOCs of a Lego set and the average number of parts in
+    these MOCs.
+    """
+
+    response = requests.get(f"{URL_REBRICK}/api/v3/lego/sets/{code}/alternates", params=params)
+    data = response.json()
+
+    moc_set_nums = []
+    moc_names = []
+    moc_num_parts = []
+    moc_urls = []
+
+    for n in range(len(data["results"])):
+        moc_set_nums.append(data['results'][n]['set_num'])
+        moc_names.append(data['results'][n]['name'])
+        moc_num_parts.append(data['results'][n]['num_parts'])
+        moc_urls.append(data['results'][n]['moc_url'])
+
+    # Dictionary containing all MOCs of one Lego set.
+    moc_dict = {
+        "set_num": moc_set_nums,
+        "name": moc_names,
+        "num_parts": moc_num_parts,
+        "instruction_url": moc_urls
+    }
+
+    mocs_df = pd.DataFrame(moc_dict)
+
+    # Sort dataframe based on the number of parts in a MOC
+    mocs_df = mocs_df.sort_values("num_parts", ascending=False)
+
+    # Count the percentage of pieces used in relation to the original set. Remove MOC's using extra pieces.
+    mocs_df["pieces_used_percentage"] = round((mocs_df["num_parts"] / parts) * 100, 1)
+    mocs_df = mocs_df.query("pieces_used_percentage < 100")
+
+    # Remove "-1" from the end of the set number before creating csv-file.
+    code = code.split("-")[0]
+    mocs_df.to_csv(f"{code}-MOCs.csv")
+
+    return (len(mocs_df), (mocs_df["num_parts"].mean()))
+
+def get_lego_info():
+    """Writes a csv file listing the information of all the Lego sets given in 'my_lego_sets.csv'.
+    Also creates separate files for every set containing a list of the MOCs for that set.
+    Returns a DataFrame containing data of all Lego sets.
+    """
+
+    # Read a file containing Lego set numbers and convert the content to a list.
+    my_own_legos = pd.read_csv("my_lego_sets.csv")
+    own_legos_list = my_own_legos["Set number"].to_list()
+
+    # Parameters for the Rebrick API call.
+    params_rb = {
+        "key": keys.APIKEY_REBRICK
+    }
+
+    # Lists for storing the data received via the Rebrick and Brickset API calls.
+    set_numbers = []
+    names = []
+    parts = []
+    instructions = []
+    num_mocs_list = []
+    avg_moc_parts_list = []
+
+    # For every set number get all necessary info using Rebrick and Brickset APIs.
+    for code in own_legos_list:
+        rebrick_code = str(code) + "-1"
+
+        response_rb = requests.get(f"{URL_REBRICK}/api/v3/lego/sets/{rebrick_code}", params=params_rb)
+
+        data_rb = response_rb.json()
+
+        # Check that JSON file contains expected data. If not, continue to the next set number.
+        try:
+            data_point = data_rb["num_parts"]
+        except (KeyError, IndexError, TypeError):
+            continue
+
+        # Get all MOCs of the set and write them in a csv file.
+        num_mocs, avg_moc_parts = get_mocs(rebrick_code, data_rb["num_parts"], params_rb)
+
+        # Parameters for Brickset API call.
+        params_bs = {
+            "apiKey": keys.APIKEY_BRICKSET,
+            "setNumber": code
+        }
+
+        response_bs = requests.get(f"{URL_BRICKSET}/getInstructions2", params=params_bs)
+        data_bs = response_bs.json()
+
+        # Store the data in lists
+        set_numbers.append(str(code))
+        names.append(data_rb["name"])
+        parts.append(data_rb["num_parts"])
+        num_mocs_list.append(num_mocs)
+        avg_moc_parts_list.append(avg_moc_parts)
+        instructions.append(data_bs["instructions"][0]["URL"])
+
+    # Dictionary for storing the lists created in the for-loop before. Used to create a pandas Dataframe.
+    lego_dict = {
+        "set_num": set_numbers,
+        "name": names,
+        "num_parts": parts,
+        "num_mocs": num_mocs_list,
+        "avg_moc_parts": avg_moc_parts_list,
+        "instruction_url": instructions
+    }
+
+    # Create a Pandas Dataframe from the dictionary, sort it by set number and write to a csv file
+    legos_df = pd.DataFrame(lego_dict)
+    legos_df = legos_df.sort_values("set_num")
+    legos_df.to_csv("lego_info.csv")
+
+    return legos_df
+
---
+++ plots.py
@@ -0,0 +1,71 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+def create_plots(legos_df):
+    """Creates some plots based on the data in the DataFrame and saves them in the file 'lego_plots.png'. """
+
+    plt.style.use("seaborn-v0_8")
+
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(nrows=2,
+                                                 ncols=2,
+                                                 figsize=(20, 20))
+    # Plot the data
+    ax1.bar(legos_df["set_num"],
+                      legos_df["num_parts"])
+
+    # Customize the plot
+    ax1.set(title="Number of Parts per Set",
+           xlabel="Set Number",
+           ylabel="Parts")
+
+    ax1.grid(axis='y', linestyle="--")
+    ax1.set_xticks(ax1.get_xticks(), ax1.get_xticklabels(), rotation=20, ha='right', fontsize=8)
+
+
+    # Plot the data
+    ax2.bar(legos_df["set_num"],
+            legos_df["num_mocs"])
+
+    # Customize the plot
+    ax2.set(title="Number of MOCs per Set",
+            xlabel="Set Number",
+            ylabel="MOCs")
+
+    ax2.grid(axis='y', linestyle="--")
+    ax2.set_xticks(ax2.get_xticks(), ax2.get_xticklabels(), rotation=20, ha='right', fontsize=8)
+
+    # Set mean line
+    ax2.axhline(legos_df["num_mocs"].mean(),
+                linestyle='--', color="red")
+
+    # Plot the data
+    ax3.bar(legos_df["set_num"],
+            legos_df["avg_moc_parts"])
+
+    # Customize the plot
+    ax3.set(title="Average Number of parts in a MOC per Set",
+            xlabel="Set Number",
+            ylabel="Average MOC Parts")
+
+    ax3.grid(axis='y', linestyle="--")
+    ax3.set_xticks(ax3.get_xticks(), ax3.get_xticklabels(), rotation=20, ha='right', fontsize=8)
+
+    # Plot the data
+    ax4.scatter(x=legos_df["num_parts"],
+                y=legos_df["avg_moc_parts"])
+
+    # Customize the plot
+    ax4.set(title="",
+            xlabel="Number of Parts",
+            ylabel="Average MOC Parts")
+
+    ax4.grid(axis='y', linestyle="--")
+    ax4.set_xticks(ax4.get_xticks(), ax4.get_xticklabels(), rotation=20, ha='right', fontsize=8)
+
+    # Set trend line
+    sns.regplot(x=legos_df["num_parts"], y=legos_df["avg_moc_parts"], ci=False, line_kws={'color': 'red'}, ax=ax4)
+
+    # Save the figure as a file
+    fig.savefig("lego_plots.png")
+
